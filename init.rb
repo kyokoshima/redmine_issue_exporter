84df6f3052ec265bd46e570554e49d847f3c39d9
yokoshima <k.yokoshima@icd.co.jp>
2012-11-27 10:30:22 +0900

    modify unique plugin name"

--- init.rb
+++ init.rb
@@ -1,13 +1,13 @@
 require_dependency 'hooks.rb'
-Redmine::Plugin.register :issue_exporter do
-  name 'Issue Exporter plugin'
-  author 'Author name'
+Redmine::Plugin.register :redmine_issue_exporter do
+  name 'Redmine Issue Exporter plugin'
+  author 'Kenichi Yokoshima'
   description 'This is a plugin for Redmine'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
 
-	project_module :issue_exporter do
+	project_module :redmine_issue_exporter do
 			permission :view_issue_exporter, :index => [:index]
 	end
 end
